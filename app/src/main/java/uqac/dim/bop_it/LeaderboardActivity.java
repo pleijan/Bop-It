fe2aa091b527dade3716f73880e41e8b017b5ae6
pleijan <72879538+pleijan@users.noreply.github.com>
2022-03-25 10:03:37 -0400

    creation LeaderboardActivity et recuperation liste de tout les score

---
+++ app/src/main/java/uqac/dim/bop_it/LeaderboardActivity.java
@@ -0,0 +1,37 @@
+package uqac.dim.bop_it;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import uqac.dim.bop_it.data.Score;
+import uqac.dim.bop_it.data.ScoreBD;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    private ScoreBD sbd;
+    private Score score;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.leaderboard);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        sbd = ScoreBD.getDatabase(getApplicationContext());
+
+        List<Score> scores = sbd.scoreDao().getAllScore();
+
+        Log.i("DIM",scores.toString());
+
+    }
+
+
+}
