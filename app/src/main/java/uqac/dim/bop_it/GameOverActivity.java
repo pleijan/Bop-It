f832fb1937ccfe4ad1406616c441a2e2676b6e16
pleijan <72879538+pleijan@users.noreply.github.com>
2022-04-14 14:38:45 -0400

    mise en pages leaderboard

--- app/src/main/java/uqac/dim/bop_it/GameOverActivity.java
+++ app/src/main/java/uqac/dim/bop_it/GameOverActivity.java
@@ -5,12 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Iterator;
 import java.util.List;
 
 import uqac.dim.bop_it.data.Score;
@@ -41,11 +45,32 @@ public class GameOverActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.timerFinal)).setText(String.valueOf(timer));
 
         sbd = ScoreBD.getDatabase(getApplicationContext());
+        sbd.scoreDao().addScore(new Score(pseudo,timer));
         List<Score> scores = sbd.scoreDao().getAllScore();
 
-        sbd.scoreDao().addScore(new Score(pseudo,timer));
+        GridLayout grille = findViewById(R.id.grille_score);
 
-        Log.i("DIM",scores.toString());
+
+        Iterator<Score> it = scores.iterator();
+
+        while(it.hasNext()) {
+
+            TextView pseudoView = new TextView(this);
+            TextView scoreView = new TextView(this);
+
+            Score score = it.next();
+
+            pseudoView.setText(score.getPseudo());
+            scoreView.setText(String.valueOf(score.getTemps()));
+            pseudoView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            pseudoView.setWidth(200);
+
+            scoreView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+
+            grille.addView(pseudoView);
+            grille.addView(scoreView);
+
+        }
     }
 
     public void PlayAgain(View view) {
