64280d6f78a6725bb3f2839515d6ead4dd47107e
Ilyas <Ilyaskam2020@gmail.com>
2022-04-23 19:34:45 -0400

    MediaPlayerService + SwipeGesture V2

--- app/src/main/java/uqac/dim/bop_it/SwipeTest.java
+++ app/src/main/java/uqac/dim/bop_it/SwipeTest.java
@@ -1,178 +1,121 @@
 package uqac.dim.bop_it;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-;
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout relativeLayout;
+    TextView textView;
+    SwipeListener swipelistener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SwipeTest#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SwipeTest extends Fragment implements View.OnTouchListener {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private SwipeCallback swipeCallback;
-    private SwipeSingleCallback swipeSingleCallback;
-    private SwipeDirection detectSwipeDirection;
-
-    float x1, x2, y1, y2;
-    View view;
-
-    private static SwipeTest newInstance(){
-        return new SwipeTest();
-    }
-
-    public static void detect( View view, SwipeTest.SwipeCallback swipeCallback  ){
-        SwipeTest evt = SwipeTest.newInstance();
-        evt.swipeCallback = swipeCallback;
-        evt.view = view;
-        evt.detect();
-    }
-
-    public static void detectTop( View view, SwipeTest.SwipeSingleCallback swipeSingleCallback){
-        SwipeTest evt = SwipeTest.newInstance();
-        evt.swipeSingleCallback = swipeSingleCallback;
-        evt.view = view;
-        evt.detectSingle(SwipeTest.SwipeDirection.TOP);
-    }
-
-    public static void detectRight( View view, SwipeTest.SwipeSingleCallback swipeSingleCallback){
-        SwipeTest evt = SwipeTest.newInstance();
-        evt.swipeSingleCallback = swipeSingleCallback;
-        evt.view = view;
-        evt.detectSingle(SwipeTest.SwipeDirection.RIGHT);
-    }
-
-    public static void detectBottom( View view, SwipeTest.SwipeSingleCallback swipeSingleCallback){
-        SwipeTest evt = SwipeTest.newInstance();
-        evt.swipeSingleCallback = swipeSingleCallback;
-        evt.view = view;
-        evt.detectSingle(SwipeTest.SwipeDirection.BOTTOM);
-    }
-    public static void detectLeft( View view, SwipeTest.SwipeSingleCallback swipeSingleCallback){
-        SwipeTest evt = SwipeTest.newInstance();
-        evt.swipeSingleCallback = swipeSingleCallback;
-        evt.view = view;
-        evt.detectSingle(SwipeTest.SwipeDirection.LEFT);
-    }
-
-    private void detect( ){
-        view.setOnTouchListener(this);
-    }
-
-    private void detectSingle( SwipeTest.SwipeDirection direction ){
-        this.detectSwipeDirection = direction;
-        view.setOnTouchListener(this);
-    }
 
     @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        switch (event.getAction() ){
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-                y2 = event.getY();
-                SwipeTest.SwipeDirection direction =null;
-
-                float xDiff = x2-x1;
-                float yDiff = y2- y1;
-                if( Math.abs(xDiff) > Math.abs(yDiff) ){
-                    if (x1<x2){
-                        direction = SwipeTest.SwipeDirection.RIGHT;
-                    }else{
-                        direction = SwipeTest.SwipeDirection.LEFT;
-                    }
-                }else {
-                    if (y1 > y2) {
-                        direction = SwipeTest.SwipeDirection.TOP;
-                    } else {
-                        direction = SwipeTest.SwipeDirection.BOTTOM;
-                    }
-                }
-
-                // Only trigger the requested event only if there
-                if ( detectSwipeDirection !=null && swipeSingleCallback !=null ){
-                    if ( direction == detectSwipeDirection ){
-                        swipeSingleCallback.onSwipe();
-                    }
-                }else{
-                    if ( direction == SwipeTest.SwipeDirection.RIGHT ){
-                        swipeCallback.onSwipeRight();
-                    }
-                    if ( direction == SwipeTest.SwipeDirection.LEFT ){
-                        swipeCallback.onSwipeLeft();
-                    }
-                    if ( direction == SwipeTest.SwipeDirection.TOP ){
-                        swipeCallback.onSwipeTop();
-                    }
-                    if ( direction == SwipeTest.SwipeDirection.BOTTOM ){
-                        swipeCallback.onSwipeBottom();
-                    }
-                }
-
-                break;
-        }
-        return false;
-    }
-
-    public enum SwipeDirection{
-        TOP, RIGHT, BOTTOM, LEFT
-    }
-
-    public interface SwipeCallback{
-        public void onSwipeTop();
-        public void onSwipeRight();
-        public void onSwipeBottom();
-        public void onSwipeLeft();
-    }
-
-    public interface SwipeSingleCallback{
-        public void onSwipe();
-    }
-    public SwipeTest() {
-
-
-    }
-    // TODO: Rename and change types and number of parameters
-    public static SwipeTest newInstance(String param1, String param2) {
-        SwipeTest fragment = new SwipeTest();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.relativelayout);
+        textView = findViewById(R.id.text_view);
+
+        swipelistener = new SwipeListener(relativeLayout);
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swipe_test, container, false);
+    private class SwipeListener implements View.OnTouchListener{
+        GestureDetector gestureDetector;
+
+
+        //constructeur
+        SwipeListener(View view){
+            int threshold = 100;
+            int velocity_threshold = 100;
+            GestureDetector.SimpleOnGestureListener listener =
+                    new GestureDetector.SimpleOnGestureListener(){
+                        @Override
+                        public boolean onDown(MotionEvent e) {
+
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                            float xDiff = e2.getX() - e1.getX();
+                            float yDiff = e2.getY() - e1.getY();
+                            List<String> list1 = new ArrayList<>();
+                            list1.add("Swipe Top");
+                            list1.add("Swipe Bottom");
+                            list1.add("Swipe Left");
+                            list1.add("Swipe Right");
+                            Random rand = new Random();
+                            String actionSwipe = list1.get(rand.nextInt(list1.size()));
+                            textView.setText(actionSwipe);
+                            try{
+                                if(Math.abs(xDiff)>Math.abs(yDiff)){
+                                    if(Math.abs(xDiff)>threshold && Math.abs(velocityX)>velocity_threshold){
+                                        //if(actionSwipe.contains("Swipe Right")){
+                                        if (xDiff > 0) {
+                                            showToast("Swiped right");
+                                        }
+                                        //}
+                                        // if(actionSwipe.contains("Swipe Left"))  {
+                                        if(xDiff < 0) {
+                                            showToast("Swiped left");
+                                        }
+                                    }
+                                    return true;
+                                }
+                                // }
+                                else{
+                                    if(Math.abs(yDiff)>threshold && Math.abs(velocityY)>velocity_threshold){
+                                        //if(actionSwipe.contains("Swipe Down")) {
+                                        if (yDiff > 0) {
+                                            showToast("Swiped Down");
+                                        }
+                                        //}
+                                        // if(actionSwipe.contains("Swipe Top")){
+                                        if(yDiff > 0){
+                                            showToast( "Swiped top");
+                                        }
+                                    }
+                                    return true;
+                                }
+                                //}
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                            return false;
+                        }
+                    };
+            gestureDetector = new GestureDetector(listener);
+
+            view.setOnTouchListener(this);
+
+        }
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            return gestureDetector.onTouchEvent((motionEvent));
+        }
+
+
     }
+    public void showToast( String message ){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
 }
