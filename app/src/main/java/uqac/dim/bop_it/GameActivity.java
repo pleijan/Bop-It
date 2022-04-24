aefb62f1754e08799be1f38c928453ab8683e563
berlingott <simsimondu@gmail.com>
2022-04-24 10:50:43 -0400

    button

--- app/src/main/java/uqac/dim/bop_it/GameActivity.java
+++ app/src/main/java/uqac/dim/bop_it/GameActivity.java
@@ -17,27 +17,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
+import butterknife.internal.ListenerClass;
+
 public class GameActivity extends AppCompatActivity {
     private TextView CountDownText;
     private long timeLeftInMilliseconds = 300000; // Time in milliseconds
     private boolean timerIsRunning ;
     private TextView actionRequiredText;
-    private int actionSucceed  = 0;
+    private int actionSucceed  = 0; // UTILISER POUR LE POINTAGE FINAL
 
+    /**
+     * type de lactivite lancee
+     *
+     */
     private enum ActionRequired {
-        BOPIT,
+        BOPITCENTRE,
+        BOPITHAUTDROITE,
+        BOPITHAUTGAUCHE,
+        BOPITBASGAUCHE,
+        BOPITBASDROITE,
         NONE
     }
 
-
     private ActionRequired actionRequired = ActionRequired.NONE;
-
     String pseudo;
     int timer;
-
+    /**
+     * à la création de l'activite
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.page_de_jeu);
@@ -53,39 +63,45 @@ public class GameActivity extends AppCompatActivity {
 
         //Ajout de l'action sur le bouton pour le jeu
 
-        final Button button = (Button) findViewById(R.id.BopITButton);
-        button.setOnClickListener(new View.OnClickListener() {
-                                      public void onClick(View v) {
-                                          if (actionRequired == ActionRequired.BOPIT ){
-                                              actionSucceed++;
-                                              askAndLaunchRandomActions();
-                                          }
-                                          else {
-                                              gameOver(v);
-                                          }
-                                      }
-                                  });
+        /**
+         * Si le bouton es cliqué, vérifie si c'était bel et bien l'action requise
+         * Si oui, lance une autre activitée au hasard
+         * sinon, fin de la partie (
+         */
+        Button bopItButtonCentral = (Button)findViewById(R.id.bopITButtonCentral); //todo trouver bouton
+        Button bopItButtonHautGauche = (Button)findViewById(R.id.bopITButtonHautGauche); //todo trouver bouton
+        Button bopItButtonHautDroite = (Button)findViewById(R.id.bopITButtonHautDroite); //todo trouver bouton
+        Button bopItButtonBasGauche = (Button)findViewById(R.id.bopITButtonBasGauche); //todo trouver bouton
+        Button bopItButtonBasDroite = (Button)findViewById(R.id.BopITButtonBasDroite); //todo trouver bouton
+
+        /**
+         * Creation de tous les boutons, puis on les cache pour l'activité
+         * Bouton
+         */
+        bopItButtonCentral.setVisibility(View.GONE);
+        bopItButtonHautDroite.setVisibility(View.GONE);
+        bopItButtonHautGauche.setVisibility(View.GONE);
+        bopItButtonBasGauche.setVisibility(View.GONE);
+        bopItButtonBasDroite.setVisibility(View.GONE);
+
+        /**
+         * Preparation du jeu terminé, lancement de la partie
+         */
         startGame();
     }
 
+    /**
+     * Lancement de la partie, lance le premier timer et la première activité au hasard
+     *
+     */
     private void startGame() {
         CountDownText = findViewById(R.id.countdowm_timer);
         actionRequiredText = findViewById(R.id.AskedAction);
         timerIsRunning = true;
         startTimer();
         //lancement timer
-
         timer=232;
-
        askAndLaunchRandomActions(); // les actions utilisateur déterminerons si on relance une actiondemandé
-
-    }
-
-    public void gameOver(View view) {
-        Intent intent = new Intent(this, GameOverActivity.class);
-        intent.putExtra("pseudo",pseudo);
-        intent.putExtra("timer",timer);
-        startActivity(intent);
     }
 
     public void pauseMenu(View view) {
@@ -111,7 +127,7 @@ public class GameActivity extends AppCompatActivity {
     }
 
     /**
-     *
+     * mettre a jour le countdown timer
      */
     public void updateTimer(){
         int minutes = (int) timeLeftInMilliseconds / 60000;
@@ -129,23 +145,26 @@ public class GameActivity extends AppCompatActivity {
     }
 
     /**
-     *
+     *selection et lancement de la prochaine action du jeu
      */
     public void askAndLaunchRandomActions(){
-        final int random = new Random().nextInt(2) + 1; // from 1 to 1 (random 1 = 0)
+        int random = new Random().nextInt(5) + 1; // from 1 to 1 (random 1 = 0)
        //selon le random généré, choisi une fonction
         switch(random) {
             case 1:
-                bopItAction();//demande de pousser le bouton
+                bopItActionCENTRE();//demande de pousser le bouton
                 break;
             case 2:
-                bopItActionMaisPourTest();
+                bopItActionHAUTGAUCHE();
                 break;
             case 3:
+                bopItActionHAUTDROITE();
                 break;
             case 4:
+                bopItActionBASGAUCHE();
                 break;
             case 5:
+                bopItActionBASDROITE();
                 break;
             case 6:
                 break;
@@ -157,13 +176,112 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    public void bopItAction(){
-        actionRequiredText.setText("Bop-IT!");
-        actionRequired = ActionRequired.BOPIT; //TODO mis a none a des din de test, doit etre remis a BOPIT apres, note au cas
-    }
-    public void bopItActionMaisPourTest(){
-        actionRequiredText.setText("Fait rien");
-        actionRequired = ActionRequired.NONE; //TODO mis a none a des din de test, doit etre remis a BOPIT apres, note au cas
-    }
+    /**
+     * Fonction bouton lorsque la fonction de hasard choisi l'un des boutons est choisi
+     * elles remettent le bouton visible et lui met un OnClickListener pour verifier l'action lorsque cliquer
+     */
 
+    public void bopItActionCENTRE(){
+        actionRequiredText.setText("Bop-IT!");
+        Button bopItButtonCentral = (Button)findViewById(R.id.bopITButtonCentral); //todo trouver bouton
+        bopItButtonCentral.setVisibility(View.VISIBLE);
+
+        bopItButtonCentral.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (verificationDesAction(ActionRequired.BOPITCENTRE,v) ){
+                    bopItButtonCentral.setVisibility(View.GONE);
+                    askAndLaunchRandomActions();
+                }
+            }
+        });
+
+        actionRequired = ActionRequired.BOPITCENTRE;
+    }
+    public void bopItActionHAUTGAUCHE(){
+        actionRequiredText.setText("Bop-IT!");
+        Button bopItButtonHautGauche = (Button)findViewById(R.id.bopITButtonHautGauche);
+        bopItButtonHautGauche.setVisibility(View.VISIBLE);
+        bopItButtonHautGauche.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (verificationDesAction(ActionRequired.BOPITHAUTGAUCHE,v) ){
+                    bopItButtonHautGauche.setVisibility(View.GONE);
+                    askAndLaunchRandomActions();
+                }
+            }
+        });
+        actionRequired = ActionRequired.BOPITHAUTGAUCHE;
+    }
+    public void bopItActionHAUTDROITE(){
+        actionRequiredText.setText("Bop-IT!");
+        Button bopItButtonHautDroite = (Button)findViewById(R.id.bopITButtonHautDroite);
+        bopItButtonHautDroite.setVisibility(View.VISIBLE);
+
+        bopItButtonHautDroite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (verificationDesAction(ActionRequired.BOPITHAUTDROITE,v)){
+                    bopItButtonHautDroite.setVisibility(View.GONE);
+                    askAndLaunchRandomActions();
+                }
+            }
+        });
+        actionRequired=ActionRequired.BOPITHAUTDROITE;
+    }
+    public void bopItActionBASGAUCHE(){
+        actionRequiredText.setText("Bop-IT!");
+        Button bopItButtonBasGauche = (Button)findViewById(R.id.bopITButtonBasGauche);
+        bopItButtonBasGauche.setVisibility(View.VISIBLE);
+
+        bopItButtonBasGauche.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (verificationDesAction(ActionRequired.BOPITBASGAUCHE,v) ){
+                    bopItButtonBasGauche.setVisibility(View.GONE);
+                    askAndLaunchRandomActions();
+                }
+            }
+        });
+        actionRequired = ActionRequired.BOPITBASGAUCHE;
+    }
+    public void bopItActionBASDROITE(){
+        actionRequiredText.setText("Bop-IT!");
+        Button bopItButtonBasdroite = (Button)findViewById(R.id.BopITButtonBasDroite);
+        bopItButtonBasdroite.setVisibility(View.VISIBLE);
+
+        findViewById(R.id.BopITButtonBasDroite).setVisibility(View.VISIBLE);
+        bopItButtonBasdroite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (verificationDesAction(ActionRequired.BOPITBASDROITE,v) ){
+                    bopItButtonBasdroite.setVisibility(View.GONE);
+                    askAndLaunchRandomActions();
+                }
+            }
+        });
+        actionRequired = ActionRequired.BOPITBASDROITE;
+
+    }
+    /**
+     * Verification si l'action est
+     * @param actionhasard type de l'action réalisé
+     * @param v view
+     * @return true si l'action est la bonne, false si laction realise est mauvaise
+     */
+    public boolean verificationDesAction(ActionRequired actionhasard, View v){
+        if (actionRequired == actionhasard){
+            actionSucceed++;
+            return true;
+        }
+        else{
+            gameOver(v);
+            return false;
+        }
+    }
+    /**
+     * game over, transfert du score à la prochaine activité
+     * @param view
+     */
+    public void gameOver(View view) {
+        Intent intent = new Intent(this, GameOverActivity.class);
+        intent.putExtra("pseudo",pseudo);
+        intent.putExtra("timer",timer);
+        startActivity(intent);
+    }
 }
